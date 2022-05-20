916cd92437d76c072fb49f4e49a4db6e7202283c
crizzo <crizzo@Celian-Grimoire.local>
2022-05-19 21:19:57 -0700

    Updates

---
+++ precache-manifest.068a1cdc77498fff091721ea68ac2efe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cddbd22558c6070e30ba",
+    "url": "/my-app/static/css/main.3b332ee1.chunk.css"
+  },
+  {
+    "revision": "cddbd22558c6070e30ba",
+    "url": "/my-app/static/js/main.cddbd225.chunk.js"
+  },
+  {
+    "revision": "c972895fac6c9db70f84",
+    "url": "/my-app/static/js/1.c972895f.chunk.js"
+  },
+  {
+    "revision": "300a2b6d799e761fbdfb",
+    "url": "/my-app/static/js/2.300a2b6d.chunk.js"
+  },
+  {
+    "revision": "796b83abe7e2e37964ed",
+    "url": "/my-app/static/js/runtime~main.796b83ab.js"
+  },
+  {
+    "revision": "6df1f8a7d4f8f2ae8e701634c254ac7c",
+    "url": "/my-app/index.html"
+  }
+];
